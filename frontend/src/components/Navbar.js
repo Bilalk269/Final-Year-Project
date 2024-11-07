2a0e5c288243323f842c0ea53f9ca975b0d49020
Ayeshaqazi23 <ayeshaqazi845@gmail.com>
2024-11-07 13:14:17 +0500

    testing

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+      <img src="/components/image 14.png"  className="logo-image" />
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
